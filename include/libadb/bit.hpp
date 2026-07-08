2c1fe5330c7300df5e696e4ea2d442b26378797a
NkosiDaniel <xxxenouniverse@gmail.com>
2026-07-08 15:16:25 -0400

    Documentation for bit.hpp

--- include/libadb/bit.hpp
+++ include/libadb/bit.hpp
@@ -8,23 +8,24 @@
 #include "./types.hpp"
 
 namespace adb {
-    // Takes bytes and constructs an object from those bytes
+    // Takes a pointer of bytes and constructs an object of a given type with those bytes.
     template <class To>
     To from_bytes(const std::byte* bytes) {
         To ret;
         std::memcpy(&ret, bytes, sizeof(To));
         return ret;
     }
-    // Cast object as a pointer to bytes
+    // Cast an object of a type to a pointer to bytes.
     template <class From>
     std::byte* as_bytes(From& from) {
         return reinterpret_cast<std::byte*>(&from);
     }
-    // Overloaded function for const arguments
+    // Cast an object of a type to a pointer to bytes.
     template <class From>
     const std::byte* as_bytes(const From& from) {
         return reinterpret_cast<const std::byte*>(&from);
     }
+    // Convert an object of a type to a 16-byte array of 128 bits.
     template <class From>
     byte128 to_byte128(From src) {
         byte128 ret{}; // initialize all elements to 0
@@ -32,15 +33,18 @@ namespace adb {
         std::memcpy(&ret, &src, sizeof(From));
         return ret;
     }
+    // Convert an object of a type to an 8-byte array of 64 bits.
     template <class From>
     byte64 to_byte64(From src) {
         byte64 ret{};
         std::memcpy(&ret, &src, sizeof(From));
         return ret;
     }
+    // Cast a pointer to bytes to a pointer to characters (a string).
     inline std::string_view to_string_view(const std::byte* data, std::size_t size) {
         return {reinterpret_cast<const char*>(data), size };
     }
+    // Cast a pointer to bytes to a pointer to characters (a string).
     inline std::string_view to_string_view(const std::vector<std::byte>& data) {
         return to_string_view(data.data(), data.size());
     }
