38985222210f61694fd1286eb9d5c21762156b11
NkosiDaniel <xxxenouniverse@gmail.com>
2026-07-08 15:07:02 -0400

    Finished documenting process.hpp for now.

--- include/libadb/process.hpp
+++ include/libadb/process.hpp
@@ -65,34 +65,49 @@ namespace adb {
             const registers& get_registers() const { return *registers_; }
             // Write to debug registers or a general purpose register.
             void write_user_area(std::size_t offset, std::uint64_t data);
-
+            // Write to all floating point registers.
             void write_fprs(const user_fpregs_struct& fprs);
+            // Write to all general purpose registers.
             void write_gprs(const user_regs_struct& gprs);
-            // obtaining the program counter
+            // Return the program counter from the %rip register.
             virt_addr get_pc() const {
                 return virt_addr{
                     get_registers().read_by_id_as<std::uint64_t>(register_id::rip)
                 };
             }
-            // setting the program counter
+            // Set the program counter by writing to the %rip register.
             void set_pc(virt_addr address)
             {
                 get_registers().write_by_id(register_id::rip, address.addr());
             }
-            // breakpoint_site stuff
+            /*
+             * Create a breakpoint site at the given address.
+             * Only one breakpoint site may exist at the same address.
+             */
             breakpoint_site& create_breakpoint_site(virt_addr address);
-            // to return our breakpoint sites. Const overload to maintain const correctness.
+            // Return collection of breakpoint sites.
             stopPoint_collection<breakpoint_site>& breakpoint_sites() { return breakpoint_sites_; }
+            // Return collection of breakpoint sites. A const overload to maintain const correctness.
             const stopPoint_collection<breakpoint_site>& breakpoint_sites() const { return breakpoint_sites_; }
-
-            // support for stepping over machine instructions
+            /*
+             * Step over a single instruction within the process. Before performing a single
+             * step, a check is made to see if a breakpoint is enabled at the current program counter. If so, then the breakpoint is disabled and re-enabled again later.
+             */
             adb::stop_reason step_instruction();\
 
-            // memory operations
+            // Read memory from a virtual address and to a specified amount.
             std::vector<std::byte> read_memory(virt_addr address, std::size_t amount) const;
+            /* Read memory while replacing int3 instructions that are necessary for
+            *  breakpoints with the original data at each address within a range.
+            */
             std::vector<std::byte> read_memory_without_traps(virt_addr address, std::size_t amount) const;
-            void write_memory(virt_addr address, span<const std::byte> data); // make sure we can't accidentally modify the data to which span points to
-
+            /*
+             * Loop until all the data that is given is written to the desired memory
+             * address. Data is written 8 bytes at a time and in the case that there isn't at
+             * least 8 bytes of data to write, then do a partial memory write.
+             */
+            void write_memory(virt_addr address, span<const std::byte> data);
+            // Read from a given memory address as a different type.
             template <class T>
             T read_memory_as(virt_addr address) const {
                 auto data = read_memory(address, sizeof(T)); // reads amount of memory equal to the size of the type
@@ -110,7 +125,7 @@ namespace adb {
 
             process(pid_t pid, bool terminate_on_end, bool is_attatched) : pid_(pid), terminate_on_end_(terminate_on_end),
                                                                            is_attatched(is_attatched), registers_(new registers(*this)) { }
-
+            // Used to periodically update registers.
             void read_all_registers(); // populate registers when the process halts
     };
 }
