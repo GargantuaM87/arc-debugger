f56afe60029469211f8ca3f3d2f4a727248039b4
NkosiDaniel <xxxenouniverse@gmail.com>
2026-07-08 15:24:02 -0400

    Added documentation for breakpoint_site.hpp

--- include/libadb/breakpoint_site.hpp
+++ include/libadb/breakpoint_site.hpp
@@ -15,17 +15,25 @@ namespace adb {
             breakpoint_site& operator=(const breakpoint_site&) = delete;
 
             using id_type = std::int32_t;
+            // Return the ID used for this breakpoint site.
             id_type id() const { return id_; }
-
+            // Enable a breakpoint site while saving the data at that site.
             void enable();
+            // Disable a breakpoint site while restoring the saved data at
+            // that site.
             void disable();
-
+            // Return if this current site is enabled.
             bool is_enabled() const { return is_enabled_; }
+            // Return the address at this site.
             virt_addr address() const {return address_; }
-
+            // Check if this breakpoint site is at the given virtual address.
             bool at_address(virt_addr addr) const {
                 return address_ == addr;
             }
+            /*
+             * Check if this breakpoint site is within the specified range of
+             * virtual addresses.
+             */
             bool in_range(virt_addr low, virt_addr high) const {
                 return low <= address_ and high > address_;
             }
