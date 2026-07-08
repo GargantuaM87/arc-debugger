8aae3f819f20097b59dcf1b24f75d605dfc41002
NkosiDaniel <xxxenouniverse@gmail.com>
2026-07-08 15:42:16 -0400

    Documentation for error.hpp

--- include/libadb/error.hpp
+++ include/libadb/error.hpp
@@ -8,8 +8,10 @@ namespace adb {
     class error : public std::runtime_error {
         public:
             [[noreturn]] // Function is supposed to terminate through different means than returning a value and it doesn't return control flow when exiting
+            // Throw an exception with a given message.
             static void send(const std::string& desc) { throw error(desc); }
             [[noreturn]]
+            // Add a prefix to a message describing the meaning of the 'errno' code.
             static void send_errno(const std::string& prefix) {
                 throw error(prefix + ": " + std::strerror(errno));
             }
@@ -17,5 +19,5 @@ namespace adb {
             error(const std::string& desc) : std::runtime_error(desc) {}
     };
 }
-
+// Throw an exception with a given message.
 #endif
