4280f64cf096530b63f507be97736e3bddf275f9
NkosiDaniel <xxxenouniverse@gmail.com>
2026-08-19 11:55:03 -0400

    Reformatted tools/adb

--- tools/adb.cpp
+++ tools/adb.cpp
@@ -59,12 +59,65 @@ namespace {
 // Forward Declarations
 namespace {
     // Print disassembled assembly code from machine code.
-    void print_disassembly(adb::process& process, adb::virt_addr address, std::size_t n_instructions);
     void handle_stop(adb::process& process, adb::stop_reason reason);
     std::string get_sigtrap_info(const adb::process& process, adb::stop_reason reason);
     void print_help(const std::vector<std::string>& args);
     bool is_prefix(std::string_view str, std::string_view of);
     std::vector<std::string> split(std::string_view str, char delimiter);
+}
+
+// Memory Namespace
+namespace {
+     void handle_memory_read_command(adb::process& process, const std::vector<std::string>& args) {
+        auto address = adb::to_integral<std::uint64_t>(args[2], 16);
+
+        if(!address) adb::error::send("Invalid address format");
+
+        auto n_bytes = 32; // default bytes to read
+        if(args.size() == 4) { //if user provides number of bytes to read as well
+            auto bytes_arg = adb::to_integral<std::size_t>(args[3]);
+            n_bytes = *bytes_arg;
+        }
+
+        auto data = process.read_memory(adb::virt_addr{ *address }, n_bytes);
+        // loop over data 16 bytes at a time and compute start and end ranges
+        for(std::size_t i = 0; i < data.size(); i += 16) { // make sure i is divisible by 16
+            auto start = data.begin() + i;
+            auto end = data.begin() + std::min(i + 16, data.size());
+            fmt::print("{:#016x}: {:02x}\n", *address + i, fmt::join(start, end, " "));
+        }
+    }
+
+     void handle_memory_write_command(adb::process& process, const std::vector<std::string>& args) {
+        if(args.size() != 4) {
+            print_help({ "help", "memory"});
+            return;
+        }
+
+        auto address = adb::to_integral<std::uint64_t>(args[2], 16);
+        if (!address)
+            adb::error::send("Invalid address format");
+
+        auto data = adb::parse_vector(args[3]);
+        process.write_memory(adb::virt_addr{*address}, {data.data(), data.size()});
+    }
+
+     void handle_memory_command(adb::process& process, const std::vector<std::string>& args) {
+        if(args.size() < 3) {
+            print_help( {"help", "memory"});
+            return;
+        }
+
+        if(is_prefix(args[1], "read")) {
+            handle_memory_read_command(process, args);
+        }
+        else if(is_prefix(args[1], "write")) {
+            handle_memory_write_command(process, args);
+        }
+        else {
+            print_help({"help", "memory"});
+        }
+    }
 }
 
 // Register Namespace
@@ -170,6 +223,7 @@ namespace {
         }
     }
 }
+
 // Software/Hardware Breakpoints Namespace
 namespace {
     void handle_breakpoint_command(adb::process& process, const std::vector<std::string>& args) {
@@ -239,6 +293,104 @@ namespace {
     }
 }
 
+// Watchpoint Namespace
+namespace {
+     void handle_watchpoint_list(adb::process& process, const std::vector<std::string>& args) {
+        auto stopPoint_mode_to_string = [](auto mode) { // lambda to get string representation of diff stopPoint modes
+            switch(mode) {
+                case adb::stopPoint_mode::execute: return "execute";
+                case adb::stopPoint_mode::read_write: return "read_write";
+                case adb::stopPoint_mode::write: return "write";
+                default: adb::error::send("Invalid stop point mode");
+            }
+        };
+        if(process.watchpoints().empty()) {
+            fmt::print("No watchpoints set\n");
+        }
+        // print watchpoint details to user
+        else {
+            fmt::print("Current Watchpoints:\n");
+            process.watchpoints().for_each([&](auto& point) {
+                fmt::print("{}: address = {:#x}, mode = {}, size = {}, {}\n",
+                    point.id(), point.address().addr(), stopPoint_mode_to_string(point.mode()), point.size(),
+                    point.is_enabled() ? "enabled" : "disabled");
+            });
+
+            }
+        }
+
+    void handle_watchpoint_set(adb::process& process, const std::vector<std::string>& args) {
+        if(args.size() != 5) {
+            print_help({"help", "watchpoint"});
+            return;
+        }
+        // getting the necessary information from comand line arguments
+        auto address = adb::to_integral<std::uint64_t>(args[2], 16);
+        auto mode_text = args[3];
+        auto size = adb::to_integral<std::size_t>(args[4]);
+
+        if(!address or !size or !(mode_text == "w" or mode_text == "rw" or mode_text == "x")) {
+            print_help( {"help", "watchpoint"} );
+            return;
+        }
+
+        adb::stopPoint_mode mode;
+        if(mode_text == "w")
+            mode = adb::stopPoint_mode::write;
+        else if(mode_text == "rw")
+            mode = adb::stopPoint_mode::read_write;
+        else if(mode_text == "x")
+            mode = adb::stopPoint_mode::execute;
+
+        process.create_watchpoint(adb::virt_addr{*address}, mode, *size).enable();
+    }
+
+    /*
+     * watchpoint list
+     * watchpoint set <address> <mode> <size>
+     * watchpoint enable <id>
+     * watchpoint disable <id>
+     * watchpoint delete <id>
+     */
+    void handle_watchpoint_command(adb::process& process, const std::vector<std::string>& args) {
+        if(args.size() < 2) {
+            print_help( {"help", "watchpoint"} );
+            return;
+        }
+
+        auto command = args[1];
+
+        if(is_prefix(command, "list")) {
+            handle_watchpoint_list(process, args);
+            return;
+        }
+        if(is_prefix(command, "set")) {
+            handle_watchpoint_set(process, args);
+            return;
+        }
+        if(args.size() < 3) {
+            print_help( {"help", "watchpoint"} );
+            return;
+        }
+
+        auto id = adb::to_integral<adb::watchpoint::id_type>(args[2]);
+        if(!id) {
+            std::cerr << "Command expects watchpoint ID";
+            return;
+        }
+
+        if(is_prefix(command, "enable")) {
+            process.watchpoints().get_by_id(*id).enable();
+        }
+        else if(is_prefix(command, "disable")) {
+            process.watchpoints().get_by_id(*id).disable();
+        }
+        else if(is_prefix(command, "delete")) {
+            process.watchpoints().remove_by_id(*id);
+        }
+    }
+}
+
 // Catchpoint Namespace
 namespace {
     void handle_syscall_catchpoint_command(adb::process& proc, const std::vector<std::string>& args) {
@@ -268,6 +420,51 @@ namespace {
         if(is_prefix(args[1], "syscall")) {
             handle_syscall_catchpoint_command(proc, args);
         }
+    }
+
+}
+
+// Disassemble Namespace
+namespace {
+    void print_disassembly(adb::process& process, adb::virt_addr address, std::size_t n_instructions) {
+        adb::disassembler disasm(process);
+        auto instructions = disasm.disassemble(n_instructions, address);
+        for(auto& instr : instructions) {
+            // :#018x specifier prints a hexadecimal representation of the address with enough padding to stay vertically aligned
+            fmt::print("{:#018x}: {}\n", instr.address.addr(), instr.text);
+        }
+    }
+
+    // disassemble -c <n_instructions>
+    // disassemble -a <address>
+    void handle_disassemble_command(adb::process& process, const std::vector<std::string>& args)
+    {
+        auto address = process.get_pc();
+        std::size_t n_instructions = 5; // set this to five instructions by default
+
+        auto it = args.begin() + 1;
+        // looping over to see which argument the user sent us
+        while(it != args.end()) {
+            if(*it == "-a" and it + 1 != args.end()) {
+                ++it;
+                auto opt_addr = adb::to_integral<std::uint64_t>(*it++, 16);
+                if(!opt_addr)
+                    adb::error::send("Invalid address format");
+                address = adb::virt_addr{*opt_addr};
+            }
+            else if(*it == "-c" and it + 1 != args.end()) {
+                ++it;
+                auto opt_n = adb::to_integral<std::size_t>(*it++);
+                if(!opt_n)
+                    adb::error::send("Invalid instruction count");
+                n_instructions = *opt_n;
+            }
+            else {
+                print_help( {"help", "disassemble"} );
+                return;
+            }
+        }
+        print_disassembly(process, address, n_instructions);
     }
 
 }
@@ -391,188 +588,6 @@ namespace {
     }
 
 
-
-
-    void handle_memory_read_command(adb::process& process, const std::vector<std::string>& args) {
-        auto address = adb::to_integral<std::uint64_t>(args[2], 16);
-
-        if(!address) adb::error::send("Invalid address format");
-
-        auto n_bytes = 32; // default bytes to read
-        if(args.size() == 4) { //if user provides number of bytes to read as well
-            auto bytes_arg = adb::to_integral<std::size_t>(args[3]);
-            n_bytes = *bytes_arg;
-        }
-
-        auto data = process.read_memory(adb::virt_addr{ *address }, n_bytes);
-        // loop over data 16 bytes at a time and compute start and end ranges
-        for(std::size_t i = 0; i < data.size(); i += 16) { // make sure i is divisible by 16
-            auto start = data.begin() + i;
-            auto end = data.begin() + std::min(i + 16, data.size());
-            fmt::print("{:#016x}: {:02x}\n", *address + i, fmt::join(start, end, " "));
-        }
-
-    }
-
-    void handle_memory_write_command(adb::process& process, const std::vector<std::string>& args) {
-        if(args.size() != 4) {
-            print_help({ "help", "memory"});
-            return;
-        }
-
-        auto address = adb::to_integral<std::uint64_t>(args[2], 16);
-        if (!address)
-            adb::error::send("Invalid address format");
-
-        auto data = adb::parse_vector(args[3]);
-        process.write_memory(adb::virt_addr{*address}, {data.data(), data.size()});
-    }
-
-    void handle_memory_command(adb::process& process, const std::vector<std::string>& args)
-    {
-        if(args.size() < 3) {
-            print_help( {"help", "memory"});
-            return;
-        }
-
-        if(is_prefix(args[1], "read")) {
-            handle_memory_read_command(process, args);
-        }
-        else if(is_prefix(args[1], "write")) {
-            handle_memory_write_command(process, args);
-        }
-        else {
-            print_help({"help", "memory"});
-        }
-    }
-    // disassemble -c <n_instructions>
-    // disassemble -a <address>
-    void handle_disassemble_command(adb::process& process, const std::vector<std::string>& args)
-    {
-        auto address = process.get_pc();
-        std::size_t n_instructions = 5; // set this to five instructions by default
-
-        auto it = args.begin() + 1;
-        // looping over to see which argument the user sent us
-        while(it != args.end()) {
-            if(*it == "-a" and it + 1 != args.end()) {
-                ++it;
-                auto opt_addr = adb::to_integral<std::uint64_t>(*it++, 16);
-                if(!opt_addr)
-                    adb::error::send("Invalid address format");
-                address = adb::virt_addr{*opt_addr};
-            }
-            else if(*it == "-c" and it + 1 != args.end()) {
-                ++it;
-                auto opt_n = adb::to_integral<std::size_t>(*it++);
-                if(!opt_n)
-                    adb::error::send("Invalid instruction count");
-                n_instructions = *opt_n;
-            }
-            else {
-                print_help( {"help", "disassemble"} );
-                return;
-            }
-        }
-        print_disassembly(process, address, n_instructions);
-    }
-
-    void handle_watchpoint_list(adb::process& process, const std::vector<std::string>& args) {
-        auto stopPoint_mode_to_string = [](auto mode) { // lambda to get string representation of diff stopPoint modes
-            switch(mode) {
-                case adb::stopPoint_mode::execute: return "execute";
-                case adb::stopPoint_mode::read_write: return "read_write";
-                case adb::stopPoint_mode::write: return "write";
-                default: adb::error::send("Invalid stop point mode");
-            }
-        };
-        if(process.watchpoints().empty()) {
-            fmt::print("No watchpoints set\n");
-        }
-        // print watchpoint details to user
-        else {
-            fmt::print("Current Watchpoints:\n");
-            process.watchpoints().for_each([&](auto& point) {
-                fmt::print("{}: address = {:#x}, mode = {}, size = {}, {}\n",
-                    point.id(), point.address().addr(), stopPoint_mode_to_string(point.mode()), point.size(),
-                    point.is_enabled() ? "enabled" : "disabled");
-            });
-
-            }
-        }
-    }
-
-    void handle_watchpoint_set(adb::process& process, const std::vector<std::string>& args) {
-        if(args.size() != 5) {
-            print_help({"help", "watchpoint"});
-            return;
-        }
-        // getting the necessary information from comand line arguments
-        auto address = adb::to_integral<std::uint64_t>(args[2], 16);
-        auto mode_text = args[3];
-        auto size = adb::to_integral<std::size_t>(args[4]);
-
-        if(!address or !size or !(mode_text == "w" or mode_text == "rw" or mode_text == "x")) {
-            print_help( {"help", "watchpoint"} );
-            return;
-        }
-
-        adb::stopPoint_mode mode;
-        if(mode_text == "w")
-            mode = adb::stopPoint_mode::write;
-        else if(mode_text == "rw")
-            mode = adb::stopPoint_mode::read_write;
-        else if(mode_text == "x")
-            mode = adb::stopPoint_mode::execute;
-
-        process.create_watchpoint(adb::virt_addr{*address}, mode, *size).enable();
-    }
-
-    /*
-     * watchpoint list
-     * watchpoint set <address> <mode> <size>
-     * watchpoint enable <id>
-     * watchpoint disable <id>
-     * watchpoint delete <id>
-     */
-    void handle_watchpoint_command(adb::process& process, const std::vector<std::string>& args) {
-        if(args.size() < 2) {
-            print_help( {"help", "watchpoint"} );
-            return;
-        }
-
-        auto command = args[1];
-
-        if(is_prefix(command, "list")) {
-            handle_watchpoint_list(process, args);
-            return;
-        }
-        if(is_prefix(command, "set")) {
-            handle_watchpoint_set(process, args);
-            return;
-        }
-        if(args.size() < 3) {
-            print_help( {"help", "watchpoint"} );
-            return;
-        }
-
-        auto id = adb::to_integral<adb::watchpoint::id_type>(args[2]);
-        if(!id) {
-            std::cerr << "Command expects watchpoint ID";
-            return;
-        }
-
-        if(is_prefix(command, "enable")) {
-            process.watchpoints().get_by_id(*id).enable();
-        }
-        else if(is_prefix(command, "disable")) {
-            process.watchpoints().get_by_id(*id).disable();
-        }
-        else if(is_prefix(command, "delete")) {
-            process.watchpoints().remove_by_id(*id);
-        }
-    }
-
     void handle_command(std::unique_ptr<adb::process>& process, std::string_view line) {
         auto args = split(line, ' ');
         auto command = args[0];
@@ -612,18 +627,6 @@ namespace {
         }
     }
 
-
-namespace {
-    void print_disassembly(adb::process& process, adb::virt_addr address, std::size_t n_instructions) {
-        adb::disassembler disasm(process);
-        auto instructions = disasm.disassemble(n_instructions, address);
-        for(auto& instr : instructions) {
-            // :#018x specifier prints a hexadecimal representation of the address with enough padding to stay vertically aligned
-            fmt::print("{:#018x}: {}\n", instr.address.addr(), instr.text);
-        }
-}
-
-}
 namespace {
     void handle_stop(adb::process &process, adb::stop_reason reason) {
             print_stop_reason(process, reason);
@@ -726,4 +729,5 @@ int main(int argc, const char** argv) {
     catch(const adb::error& err) {
         std::cout << err.what() << std::endl;
     }
+  }
 }
